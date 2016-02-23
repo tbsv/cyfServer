9d6c13b3a94b58885c44102046b40ca1520f3c49
Tobias Vetter <tobias.vetter@student.reutlingen-university.de>
2016-02-23 17:32:23 +0100

    Updated request header for Access-Control-Allow-Methods

--- routes/tours.js
+++ routes/tours.js
@@ -7,6 +7,7 @@ var tours = require('../controllers/tourController');
 router.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     next();
 });
 
