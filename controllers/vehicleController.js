09f9050f40273506e6323a189a0a8688009a0415
Ronald <ronald.henger@student.reutlingen-university.de>
2016-03-11 11:25:44 +0100

    neues Datum für WDD212.

--- controllers/vehicleController.js
+++ controllers/vehicleController.js
@@ -66,8 +66,8 @@ exports.post = function(req, res){
             // rhenger CLA ab 07.03.2016
             var validTimestamp = '2016-03-07T00:00:00.000Z';
         } else if (req.body.vin == 'WDD2122061B140828') {
-            // rhenger E-Kombi ab 02.02.2016 bis 25.02.2016!
-            var validTimestamp = '2016-02-02T00:00:00.000Z';
+            // rhenger E-Kombi ab 14.02.2016 bis 25.02.2016!
+            var validTimestamp = '2016-02-14T00:00:00.000Z';
         } else {
             var validTimestamp = new Date();
         }
